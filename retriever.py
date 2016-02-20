56bd7b7666cdd2721285c764e7fabced4a29d388
Juan Luis Boya García <ntrrgc@gmail.com>
2016-02-20 17:04:15 +0100

    Now with progress bar

--- retriever.py
+++ retriever.py
@@ -2,6 +2,7 @@
 from hepdata_retriever.retriever import Retriever
 import os
 import errno
+from progressbar import ProgressBar, Percentage, Bar, Widget
 
 data_dir  = '/hepdata/data/'
 temp_dir = '/hepdata/tmp/'
@@ -15,11 +16,38 @@ def makedirs(dirs):
         else:
             raise err
 
+class Label(Widget):
+    """Displays an updatable label."""
+    def __init__(self, min_length=0, starting_text=''):
+        self.min_length = min_length
+        self.change_text(starting_text)
+
+    def change_text(self, text):
+        self.text = text.ljust(self.min_length)
+
+    def update(self, pbar):
+        return ' ' + self.text + ' '
+
+class AlwaysUpdatingProgressBar(ProgressBar):
+    def _need_update(self):
+        return True
+
 makedirs(temp_dir)
 retriever = Retriever(temp_dir)
 inspire_ids = retriever.get_all_ids_in_current_system()
 
+submission_label = Label(min_length=10)
+pbar = AlwaysUpdatingProgressBar(maxval=len(inspire_ids),
+                   widgets=[
+                       Percentage(),
+                       submission_label,
+                       Bar(marker='#', left='[', right=']')
+                   ]).start()
+
 for index, inspire_id in enumerate(inspire_ids):
+    submission_label.change_text(inspire_id)
+    pbar.update(index)
+
     path = data_dir + '/' + inspire_id[-2:] + '/' + inspire_id
     if os.path.exists(path):
         continue
@@ -32,3 +60,5 @@ for index, inspire_id in enumerate(inspire_ids):
         break
     except:
         print('Ignoring %s.' % inspire_id)
+
+pbar.finish()
